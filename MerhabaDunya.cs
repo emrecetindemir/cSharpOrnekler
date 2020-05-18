27c97cd1b7f1d0b022988f5505c2a5784cb9199c
emrecetindemir <55704322+emrecetindemir@users.noreply.github.com>
2020-05-18 13:44:35 +0300

    Merhaba Dünya

---
+++ MerhabaDunya.cs
@@ -0,0 +1,11 @@
+class program
+{
+    static void Main (string [] args)
+    {
+        Console.WriteLine("Merhaba Dünya");
+        
+        Console.WriteLine("Çıkış yapmak için bir tuşa basınız.")
+
+        Console.ReadKey(); //Bir tuşa basmasını bekler
+    }
+}
