d5cfd964b4ae4a81cf0ebb51fc67d2c7d314eb33
emrecetindemir <55704322+emrecetindemir@users.noreply.github.com>
2020-05-18 13:51:07 +0300

    Değişkenin Değerini Ekrana Yazdırma

---
+++ DegiskenYazma.cs
@@ -0,0 +1,9 @@
+class program
+{
+    static void Main (string [] args)
+    {
+        int sayi = 41; //int türünde değişken oluşturup bir değer atadık.
+        Console.WriteLine("Sayı="+sayi); //değişkenin değerini ekrana yazdırdık.
+        Console.ReadKey();
+    }
+}
