702674961a290ee210952bb2337e7a4d01369413
emrecetindemir <55704322+emrecetindemir@users.noreply.github.com>
2020-05-19 17:54:57 +0300

    Bir değişken tanımlayıp ekrana yazdırma

--- 002-DegiskenYazma.cs
+++ 002-DegiskenYazma.cs
@@ -2,7 +2,7 @@ class program
 {
     static void Main (string [] args)
     {
-        int sayi = 41; //int türünde değişken oluşturup bir değer atadık.
+        int sayi = 41; //int türünde değişken tanımlayıp bir değer atadık.
         Console.WriteLine("Sayı="+sayi); //değişkenin değerini ekrana yazdırdık.
         Console.ReadKey();
     }
